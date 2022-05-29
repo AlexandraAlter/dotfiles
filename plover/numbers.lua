00b25fb01336ee07444084d420993071ccf6c8b9
AlexandraAlter <alexandra.alter@protonmail.com>
2022-05-29 13:48:01 +0100

    Added extras, and a crude time system

--- plover/numbers.lua
+++ plover/numbers.lua
@@ -2,11 +2,27 @@ local numbers = {}
 
 local pl = require'plover'
 
+local extras = {
+  ['THOEUB'] = '{^,000}',
+  ['THOUZ'] = '{^000}',
+  ['THO*UZ'] = '{,^000}',
+  ['K-PL'] = 'km',
+  ['K*PL'] = '{^km}',
+  ['HA*F'] = '1/2',
+  ['TPHA*F'] = '1/2',
+  ['TH*EURD'] = '1/3',
+  ['TPHUPBZ'] = '100',
+  ['TWOUPB'] = '{200^}',
+}
+
 function numbers.build()
   local dict = pl.Dict:new{}
 
   for i = 0, 9 do
     dict:add({'#-', i}, i)
+    dict:add({'#-', i, '-Z'}, i .. '000')
+    dict:add({'#-', i, '/', 'W-B', '/', 'THUS'}, i .. ',000')
+    dict:add({'#-', i, '/', 'PERS'}, i .. '0%')
   end
 
   for i = 0, 9 do
@@ -30,6 +46,24 @@ function numbers.build()
       th = 'rd'
     end
     dict:add({'#-', i, '*', '-T'}, i .. th)
+  end
+
+  for i = 0, 6 do
+    for j = 0, 5, 5 do
+      local n = i * 10 + j
+      local s = string.format('{^:%02u}', n)
+      if i == j then
+        dict:add({'#-', i, '-D', '-BG'}, s)
+      elseif i < j then
+        dict:add({'#-', i, '-EU', '-BG'}, s)
+      else
+        dict:add({'#-', i, j, '-BG'}, s)
+      end
+    end
+  end
+
+  for stroke, output in pairs(extras) do
+    dict:add(stroke, output)
   end
 
   return dict

0a49572b7c7c36e7944d6d39c9148a40e3fbf4ab
AlexandraAlter <alexandra.alter@protonmail.com>
2022-11-28 13:22:16 +0000

    Deleted old vim config

--- nvim/config/init.lua
+++ nvim/config/init.lua
@@ -236,9 +236,8 @@ vim.api.nvim_create_autocmd('FileType', {
 -- -- godot (gdscript)
 
 -- -- lspconfig
-local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
-local lsp = require('lspconfig')
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
+local lspconfig = require('lspconfig')
 -- lsp['eslint'].setup { capabilities = capabilities }
 
 -- }}}
